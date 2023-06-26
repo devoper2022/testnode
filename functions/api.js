34ef6fae8d344bbefc0d699b91051ce29e57d7e4
devoper2022 <106957070+devoper2022@users.noreply.github.com>
2023-06-26 16:07:48 +0530

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000;
 const router = express.Router();
+const axios = require('axios');
 
 // app.use(express.json());
 app.get('/', (req, res) => {
@@ -18,6 +19,44 @@ app.get('/temp', function(req, res) {
   countdown(res, 10)
 })
 
+var id = 'iiotqzna-brewery-v1.0.0';
+var secret = 'vZFm1C7j1Htb9JLJGmit0004VVU2dUvvemcmnlYEayX';
+var base64token = btoa(id + ':' + secret);
+var _provider = id.split('-')[0];
+var _appName = id.split('-')[1];
+var _version = id.split('-')[2];
+
+let data = JSON.stringify({
+  "grant_type": "client_credentials",
+  "appName": `${_appName}}`,
+  "appVersion": `${_version}`,
+  "hostTenant": `${_provider}`,
+  "userTenant": `${_provider}`
+});
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://gateway.eu1.mindsphere.io/api/technicaltokenmanager/v3/oauth/token',
+  headers: {
+    'X-SPACE-AUTH-KEY': `Bearer ${base64token}`,
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin':'*',
+    'Accept':'*/*'
+  },
+  data: data
+};
+
+
+app.get('/token', function (req, res) {
+  axios.request(config)
+  .then((response) => {
+    res.send(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
 
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
